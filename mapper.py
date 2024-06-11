33e87fd550c599dbc4d5551d6ca942e28fb9fd3c
Tyler Gomez Riddick <99568396+tpriddick@users.noreply.github.com>
2024-06-10 20:29:00 -0700

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 # -*-coding:utf-8 -*
-import sys 
-  
-for line in sys.stdin: 
-    line = line.strip() 
-    words = line.split() 
+import sys
+import re
+
+for line in sys.stdin:
+    line = line.strip()
+    words = line.split()
     for word in words:
-        print('%s\t%s' % (word, 1))
+        true = 0
+        for i in word:
+            if bool(re.search('[a-zA-Z]',i)) == True:
+                true += 1
+        if true == len(word):
+            print('%s\t%s' % (word,1))
