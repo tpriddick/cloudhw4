76802eab572dc9ec68b1dd363bd5610e8da6c8ea
Tyler Gomez Riddick <99568396+tpriddick@users.noreply.github.com>
2024-06-10 20:07:56 -0700

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -1,11 +1,9 @@
+#!/usr/bin/python3
+# -*-coding:utf-8 -*
 import sys 
-
-letters = set('abcdefghijklmnopqrstuvwxyz')
   
 for line in sys.stdin: 
     line = line.strip() 
     words = line.split() 
     for word in words:
-        word = word.lower()
-        if word[0] in letter:
-            print('%s\t%s' % (word, 1))
+        print('%s\t%s' % (word, 1))
