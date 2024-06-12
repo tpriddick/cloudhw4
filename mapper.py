6f5ce29a904baedfcc619ae05fe14b8d31804d2d
Tyler Gomez Riddick <99568396+tpriddick@users.noreply.github.com>
2024-06-11 23:18:16 -0700

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -4,17 +4,20 @@ import sys
 
 lets = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
-
-for line in sys.stdin:
-    line = line.strip()
-    words = line.split()
-    for word in words:
-        word = word.lower()
-        true = 0
-        for i in word:
-            if i in lets:
-                true += 1
-        if true == len(word):
-            print('%s\t%s' % (word,1))
-        else:
-            print('%s\t%s' % (word,0))
+try:
+    for line in sys.stdin:
+        line = line.strip()
+        words = line.split()
+        for word in words:
+            word = word.lower()
+            true = 0
+            for i in word:
+                if i in lets:
+                    true += 1
+            if true == len(word):
+                print('%s\t%s' % (word,1))
+            else:
+                print('%s\t%s' % (word,0))
+except Exception as e:
+    print(f"Error: {e}", file=sys.stderr)
+    sys.exit(1)
