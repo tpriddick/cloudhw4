0fb292f381e94cc2c09d7205f174989965ec0412
Tyler Gomez Riddick <99568396+tpriddick@users.noreply.github.com>
2024-06-11 23:54:37 -0700

    Update reducer.py

--- reducer.py
+++ reducer.py
@@ -15,13 +15,12 @@ for line in sys.stdin:
         count = int(count) 
     except ValueError: 
         continue
-    if count !=0:
-      if current_word == word: 
-          current_count += count 
-      else: 
-          if current_word: 
-              print('%s\t%s' % (current_word, current_count))
-          current_count = count 
-          current_word = word 
+    if current_word == word: 
+        current_count += count 
+    else: 
+        if current_word: 
+            print('%s\t%s' % (current_word, current_count))
+        current_count = count 
+        current_word = word 
 if current_word == word: 
     print('%s\t%s' % (current_word, current_count))
