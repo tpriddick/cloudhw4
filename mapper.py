10d90050836006174464855802afcf7e5797598c
Tyler Gomez Riddick <99568396+tpriddick@users.noreply.github.com>
2024-06-11 23:53:53 -0700

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -2,19 +2,13 @@
 # -*-coding:utf-8 -*
 import sys
 
-lets = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
-try:
-    for line in sys.stdin:
-        line = line.strip()
-        words = line.split()
-        for word in words:
-            word = word.lower()
-            true = 0
-            for i in word:
-                if i in lets:
-                    true += 1
-            if true == len(word):
-                print('%s\t%s' % (word,1))
-            else:
-                print('%s\t%s' % (word,0))
+for line in sys.stdin:
+    line = line.strip()
+    words = line.split()
+    for word in words:
+        word = word.lower()
+        if word.isalpha():
+            print('%s\t%s' % (word,1))
+        else:
+            print('%s\t%s' % (word,0))
