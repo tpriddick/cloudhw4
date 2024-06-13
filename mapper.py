d7aee2e7fd19c6488ba94532ed2c4439f6a8b173
Tyler Gomez Riddick <99568396+tpriddick@users.noreply.github.com>
2024-06-12 19:48:28 -0700

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 # -*-coding:utf-8 -*
+
+# Shamelessly taken from stackoverflow
+# https://stackoverflow.com/questions/22520932/python-remove-all-non-alphabet-chars-from-string/22521156#22521156
 import sys
+import re
 
 
 for line in sys.stdin:
     line = line.strip()
     words = line.split()
+    good = re.compile('[a-zA-Z]')
     for word in words:
         word = word.lower()
-        if word.isalpha():
-            print('%s\t%s' % (word,1))
-        else:
-            print('%s\t%s' % (word,0))
+        word = good.compile('',word)
+        print('%s\t%s' % (word,1))
